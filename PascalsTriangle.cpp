50d33948f8e5c260479a5bf714eb262e07e10285
ASHUTOSH TEWARI <ashutoshtewari99@gmail.com>
2022-06-06 12:22:10 +0530

    Update PascalsTriangle.cpp

--- PascalsTriangle.cpp
+++ PascalsTriangle.cpp
@@ -7,12 +7,10 @@ public:
         for( int row=0;row<numRows;row++)
         {
             // resize the number of colums in a row equal to row number
-            pascal[row].resize(row+1);
-            
-            pascal[row][0]=pascal[row][row]=1;
+            pascal[row].resize(row+1,1);
             for( int column=1;column<row;column++)
             {
-                pascal[row][column]=pascal[row-1][column-1]+pascal[row-1][column];
+            pascal[row][column]=pascal[row-1][column-1]+pascal[row-1][column];
             }
             
         }
