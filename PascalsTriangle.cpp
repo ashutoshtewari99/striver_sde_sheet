8f7146128840ccee47be5486e0894fcf86bd665e
ASHUTOSH TEWARI <ashutoshtewari99@gmail.com>
2022-06-06 11:58:33 +0530

    Update PascalsTriangle.cpp

--- PascalsTriangle.cpp
+++ PascalsTriangle.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) 
     {
-        vector<vector<int>> pascal;
-        int row=0,column;
-        for(int rows=0;row<numRows;rows++)
+        vector<vector<int>> pascal(numRows);
+
+        for( int row=0;row<numRows;row++)
         {
-            vector<int> temp(row+1,1);
-            column=0;
-            for(int column=1;column<row;column++)
+            // resize the number of colums in a row equal to row number
+            pascal[row].resize(row+1);
+            
+            pascal[row][0]=pascal[row][row]=1;
+            for( int column=1;column<row;column++)
             {
-                temp[column]=pascal[row-1][column-1]+pascal[row-1][column];
+                pascal[row][column]=pascal[row-1][column-1]+pascal[row-1][column];
             }
-            pascal.push_back(temp);
-            row++;
+            
         }
         return pascal;
     }
