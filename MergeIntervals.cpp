0f2eb72fe36fb6bce45fb5c5d369cf44a9f93822
ASHUTOSH TEWARI <ashutoshtewari99@gmail.com>
2022-06-09 00:45:16 +0530

    Update MergeIntervals.cpp

--- MergeIntervals.cpp
+++ MergeIntervals.cpp
@@ -1,21 +1,26 @@
-class Solution 
+class Solution
 {
 public:
-        vector<vector<int>> merge(vector<vector<int>>& interval) 
+    vector<vector<int>> merge(vector<vector<int>> &interval)
     {
-        int n=interval.size();
+        int n = interval.size();
         vector<vector<int>> ans;
-            
-        sort(interval.begin(),interval.end());
+
+        if (n == 0)
+        {
+            return ans;
+        }
+
+        sort(interval.begin(), interval.end());
         ans.push_back(interval[0]);
 
-        int j=0;
-            
-        for(int i=1;i<n;i++)
+        int j = 0;
+
+        for (int i = 1; i < n; i++)
         {
-            if(ans[j][1]>=interval[i][0])
+            if (ans[j][1] >= interval[i][0])
             {
-                ans[j][1]=max(ans[j][1],interval[i][1]);
+                ans[j][1] = max(ans[j][1], interval[i][1]);
             }
             else
             {
