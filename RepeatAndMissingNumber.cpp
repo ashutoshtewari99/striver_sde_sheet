cc690daa6b2e50838bb59f26030b6a1f3ab21a78
ASHUTOSH TEWARI <ashutoshtewari99@gmail.com>
2022-06-09 21:15:26 +0530

    Update RepeatAndMissingNumber.cpp

--- RepeatAndMissingNumber.cpp
+++ RepeatAndMissingNumber.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) 
+    vector<int> missingNumber(vector<int>& nums) 
     {
         int n=nums.size()+1;
         vector<int> subs(n,0);
-        int res;
+        vector<int> res;
 
         for(int i=0;i<nums.size();i++)
         {
             subs[nums[i]]++;
         }
-        for(int i=0;i<=nums.size();i++)
+       
+        for(int i=0;i<n;i++)
         {
             if(subs[i]==0 || subs[i]>1)
             {
-                res=i;
+                res.push_back(i);
             }
         }
         return res;
