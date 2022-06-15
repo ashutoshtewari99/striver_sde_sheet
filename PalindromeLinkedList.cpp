4b03e6c0dbb1945a3b9f0072d95e0fc4e34c04b5
ASHUTOSH TEWARI <ashutoshtewari99@gmail.com>
2022-06-16 02:13:11 +0530

    Create PalindromeLinkedList.cpp

---
+++ PalindromeLinkedList.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    ListNode* reverse(ListNode* ptr) 
+        {
+            ListNode* pre=NULL;
+            ListNode* nex=NULL;
+            while(ptr!=NULL) 
+            {
+                nex = ptr->next;
+                ptr->next = pre;
+                pre=ptr;
+                ptr=nex;
+            }
+            return pre;
+        }
+    
+    
+    
+    bool isPalindrome(ListNode* head)  
+    {
+        
+        
+        ListNode *slow=head;
+        ListNode *fast=head;
+        
+        while(fast->next!=NULL && fast->next->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        
+        slow->next=reverse(slow->next);
+        slow = slow->next;
+        ListNode* dummy = head;
+
+        while(slow!=NULL) 
+        {
+            if(dummy->val != slow->val) 
+            {
+                return false;
+            }
+            dummy = dummy->next;
+            slow = slow->next;
+        }
+        return true;
+        }
+};
