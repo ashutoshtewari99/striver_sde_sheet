1162fe1fcdda1f41d3c5c6cf69ebc68a8fb504f6
ASHUTOSH TEWARI <ashutoshtewari99@gmail.com>
2022-06-16 22:50:51 +0530

    Update IntersectionOfTwoLinkedList.cpp

--- IntersectionOfTwoLinkedList.cpp
+++ IntersectionOfTwoLinkedList.cpp
@@ -1,38 +1,47 @@
+// 1st Approach: Brute Force
 
-class Solution {
+class Solution
+{
 public:
-    ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) 
+    ListNode *getIntersectionNode(ListNode *head1, ListNode *head2)
     {
-        
-        //Brute Force
-                // while(headA!=NULL)
-                // {
-                //     ListNode *temp=headB;
-                //     while(temp!=NULL)
-                //     {
-                //         if(headA==temp)
-                //         {
-                //             return temp;
-                //         }
-                //         temp=temp->next;
-                //     }
-                //     headA=headA->next;
-                // }
-                // return NULL;
-        
 
-        
-        //Optimised
-        ListNode* d1 = head1;
-        ListNode* d2 = head2;
-
-        while(d1 != d2) 
+        // Brute Force
+        while (headA != NULL)
         {
-            d1 = d1 == NULL? head2:d1->next;
-            d2 = d2 == NULL? head1:d2->next;
+            ListNode *temp = headB;
+            while (temp != NULL)
+            {
+                if (headA == temp)
+                {
+                    return temp;
+                }
+                emp = temp->next;
+            }
+            headA = headA->next;
+        }
+        return NULL;
+    }
+};
+
+// 2nd Approach: Optimised
+
+class Solution
+{
+public:
+    ListNode *getIntersectionNode(ListNode *head1, ListNode *head2)
+    {
+
+        // Optimised
+        ListNode *d1 = head1;
+        ListNode *d2 = head2;
+
+        while (d1 != d2)
+        {
+            d1 = d1 == NULL ? head2 : d1->next; // if d1 is NULL assign head2 to d1 else move d1 to d1->next
+            d2 = d2 == NULL ? head1 : d2->next;
         }
 
         return d1;
-
-        }
+    }
 };
