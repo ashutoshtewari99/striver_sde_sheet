47ba5ab557c22725fecbcf562ba546e089ff99a3
ASHUTOSH TEWARI <ashutoshtewari99@gmail.com>
2022-07-02 23:37:32 +0530

    Update LeftViewofBinaryTree.cpp

--- LeftViewofBinaryTree.cpp
+++ LeftViewofBinaryTree.cpp
@@ -1,21 +1,22 @@
-void solve(Node* root,vector<int> &ans,int level){
-     //base case
-     if(root==NULL)
+void solve(Node *root, vector<int> &ans, int level)
+{
+    // base case
+    if (root == NULL)
+    {
         return;
-        
-    //we entered into a new level
-    if(level==ans.size())
+    }
+    // we entered into a new level
+    if (level == ans.size())
+    {
         ans.push_back(root->data);
-        
-    solve(root->left,ans,level+1);
-    solve(root->right,ans,level+1);
+    }
+    solve(root->left, ans, level + 1);
+    solve(root->right, ans, level + 1);
 }
 
-    
-    
 vector<int> leftView(Node *root)
 {
     vector<int> ans;
-    solve(root,ans,0);
+    solve(root, ans, 0);
     return ans;
 }
