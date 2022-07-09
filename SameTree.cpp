dcd048602f34cec28d4bc7849c641436edebe151
ASHUTOSH TEWARI <ashutoshtewari99@gmail.com>
2022-07-09 22:40:10 +0530

    Update SameTree.cpp

--- SameTree.cpp
+++ SameTree.cpp
@@ -1,27 +1,29 @@
-class Solution {
+class Solution
+{
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) 
+    bool isSameTree(TreeNode *p, TreeNode *q)
     {
-        if(p==NULL && q==NULL)
+        if (p == NULL && q == NULL)
             return true;
-        if(p==NULL || q==NULL)
+        if (p == NULL || q == NULL)
             return false;
-        if(p->val != q->val)
+        if (p->val != q->val)
             return false;
-        return isSameTree(p->left,q->left) && isSameTree( p->right, q->right);
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
 
-//faster than 100%
-class Solution {
-/* public:
-    bool isSameTree(TreeNode* p, TreeNode* q) 
-    {
-        if(p==NULL || q==NULL)
-            return (p==q);
-        
-        return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree( p->right, q->right);
-    }
-}; 
-shift+option + A = multiline comment
-*/
+// faster than 100%
+class Solution
+{
+    /* public:
+        bool isSameTree(TreeNode* p, TreeNode* q)
+        {
+            if(p==NULL || q==NULL)
+                return (p==q);
+
+            return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree( p->right, q->right);
+        }
+    };
+    shift+option + A = multiline comment
+    */
