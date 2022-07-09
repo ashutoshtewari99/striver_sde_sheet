915646add532c2e6f6d7edeffca1d8c727ede663
ASHUTOSH TEWARI <ashutoshtewari99@gmail.com>
2022-07-09 22:39:58 +0530

    Update SameTree.cpp

--- SameTree.cpp
+++ SameTree.cpp
@@ -11,3 +11,17 @@ public:
         return isSameTree(p->left,q->left) && isSameTree( p->right, q->right);
     }
 };
+
+//faster than 100%
+class Solution {
+/* public:
+    bool isSameTree(TreeNode* p, TreeNode* q) 
+    {
+        if(p==NULL || q==NULL)
+            return (p==q);
+        
+        return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree( p->right, q->right);
+    }
+}; 
+shift+option + A = multiline comment
+*/
