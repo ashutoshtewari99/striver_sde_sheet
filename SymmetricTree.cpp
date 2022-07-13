f937fabb28a2a539675d995226b3a4284d0d3177
ASHUTOSH TEWARI <ashutoshtewari99@gmail.com>
2022-07-13 23:53:15 +0530

    Create SymmetricTree.cpp

---
+++ SymmetricTree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool check(TreeNode *root1,TreeNode *root2)
+    {
+        if(!root1 && !root2)
+            return true;
+        if(!root1 || !root2 || root1->val!=root2->val)
+            return false;
+        return (check(root1->left,root2->right) && check(root1->right,root2->left));
+    }
+    bool isSymmetric(TreeNode* root) {
+        return check(root->left,root->right);
+        
+    }
+};
